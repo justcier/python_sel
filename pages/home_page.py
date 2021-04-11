ccbed6312778f5497e1a7a7a96b35a2c59903dcf
Justyna Ciereszko <jcier@softserveinc.com>
2021-04-11 22:06:25 +0200

    Added logger to pages and methods.

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.webdriver.common.by import By
 from webdriver_manager import driver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,6 +14,8 @@ class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.my_account_href_xpath = "//a[text()='My account']"
+        self.logger = logging.getLogger(__name__)
 
     def go_to_my_account_page(self):
+        self.logger.info("Go to my account page")
         self.driver.find_element(By.XPATH, self.my_account_href_xpath).click()
