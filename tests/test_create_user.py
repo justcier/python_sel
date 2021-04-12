8c97e86b36655da3244afc5d5a5a4009696a08cc
Justyna Ciereszko <jcier@softserveinc.com>
2021-04-12 23:19:12 +0200

    Added assertpy and register user tests with parametrization

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,4 +1,6 @@
 import pytest
+from assertpy import assert_that
+
 import config
 import allure
 from datetime import datetime
@@ -21,3 +23,41 @@ class TestCreateUser:
         home_page.go_to_my_account_page()
         account_page.register_user(generate_email, generate_password)
         account_page.is_logout_link_displayed()
+
+    @allure.title("Create user with existing data")
+    @allure.description(f"Test try to create user with existing data in database-> email: {config.USER_EMAIL} "
+                        f"and password: {config.USER_PASSWORD}")
+    @allure.testcase("https://jciercompany.qatouch.com/case/edit/p/Jn3a/cid/5gz9M",
+                     "TC0024 - Create user in my account page with existing data")
+    def test_create_user_with_existing_data(self):
+        home_page = HomePage(self.driver)
+        account_page = MyAccountPage(self.driver)
+        home_page.go_to_my_account_page()
+        account_page.register_user(config.USER_EMAIL, config.USER_PASSWORD)
+        error_msg = account_page.validation_msg()
+        assert_that(error_msg).is_equal_to(
+            'Error: An account is already registered with your email address. Please log in.')
+
+    @allure.title("Create user with invalid email")
+    @allure.description(f"Test try to create user with invalid email: a@b.c")
+    @allure.testcase("https://jciercompany.qatouch.com/case/view/p/Jn3a/cid/d9N3Z",
+                     "TC0025 - Create user in my account page with invalid email")
+    def test_create_user_with_invalid_email(self):
+        home_page = HomePage(self.driver)
+        account_page = MyAccountPage(self.driver)
+        home_page.go_to_my_account_page()
+        account_page.register_user("a@b.c", config.USER_PASSWORD)
+        error_msg = account_page.validation_msg()
+        assert_that(error_msg).is_equal_to('Error: Please provide a valid email address.')
+
+    @allure.title("Test param create user validation")
+    @pytest.mark.parametrize("email, password, error", [(config.USER_EMAIL, config.USER_PASSWORD, 'Error: An account is already registered with your email address. Please log in.'),
+                                                        ('a@b.c', 'xyzQ!!1234', 'Error: Please provide a valid email address.'),
+                                                        ('temail@email.com', '', 'Error: Please enter an account password.')])
+    def test_param_create_user_validation(self, email, password, error):
+        home_page = HomePage(self.driver)
+        account_page = MyAccountPage(self.driver)
+        home_page.go_to_my_account_page()
+        account_page.register_user(email, password)
+        error_msg = account_page.validation_msg()
+        assert_that(error_msg).is_equal_to(error)
